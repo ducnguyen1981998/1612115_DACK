4e12f1b14a6f0a6c9d249712a562c47d5a4817cf
1981998 <44722881+ducnguyen1981998@users.noreply.github.com>
2020-11-14 21:01:15 +0700

    Account Management
    + UI Profile
    + UI Setting

---
+++ src/components/Account Management/Profile/index.js
@@ -0,0 +1,109 @@
+import React, {Component} from 'react'
+import { StyleSheet, Text, View, Image, Alert, Button, TextInput } from 'react-native';
+import Person from '../../../../assets/img/person1.jpg'
+import Icon from 'react-native-vector-icons/Ionicons';
+import Author from '../../Main/Browse/Author/author'
+
+import Day from '../../../../assets/icon/Day.jpg';
+import Time from '../../../../assets/icon/time.jpg';
+import MostViewed from '../../../../assets/icon/mostviewed.jpg';
+
+const index = (props) => {
+    return (
+        <View style={ styles.container}>
+            <View style={ styles.containerAvatar}>
+                <Image source={Person} style={styles.imgProfile}/>
+                <Text style={styles.txtName}>Nguyen Van Duc</Text>
+                <Text style={styles.txtJob}>@ducnguyen</Text>
+            </View>
+            <View style={styles.container1}>
+                <View style={styles.containerInfo}>
+                    {/* <Icon style={styles.icon} name="ios-list-box" size={48}/> */}
+                    <Image source={Day} style={styles.icon}/>
+                    <View style={ styles.containerTxt}>
+                        <Text style={ styles.txtTitle}>Totals activity days</Text>
+                        <Text style={styles.txtInfo}>1024 day streak</Text>
+                    </View>
+                </View>
+                <View style={styles.containerInfo}>
+                    {/* <Icon style={styles.icon} name="ios-list-box" size={48}/> */}
+                    <Image source={Time} style={styles.icon}/>
+                    <View style={ styles.containerTxt}>
+                        <Text style={ styles.txtTitle}>Most activity time </Text>
+                        <Text style={styles.txtInfo}>7:00 AM</Text>
+                    </View>
+                </View>
+                <View style={styles.containerInfo}>
+                    {/* <Icon style={styles.icon} name="ios-list-box" size={48}/> */}
+                    <Image source={MostViewed} style={styles.icon}/>
+                    <View style={ styles.containerTxt}>
+                        <Text style={ styles.txtTitle}>Most viewed subject</Text>
+                        <Text style={styles.txtInfo}>N/A</Text>
+                    </View>
+                </View>                
+            </View>
+            <View style={styles.containerAuthor}>
+                <Author title={"Author"} navigation={props.navigation}/>
+            </View>
+             
+        </View>
+    )
+}
+
+export default index
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems:'center'
+    },
+    containerAvatar:{
+        flex:0.6,
+        alignItems:'center',
+        marginTop: 30,   
+    },
+    containerAuthor:{
+        flex:0.5
+    },
+    imgProfile:{
+        height:120,
+        width:120
+    },
+    txtName:{
+        // color: "white",
+        fontSize: 22,
+        marginTop:12
+    },
+    txtJob:{
+         color: "#626FB4",
+        fontSize: 16,
+        marginTop:4
+    },
+
+    containerInfo:{
+        flexDirection:'row',
+        marginLeft:30
+    },
+    icon:{
+        height:40,
+        width:36,
+    },
+    containerTxt:{
+        marginLeft: 10
+    },
+    txtTitle:{
+        color: "#626FB4",
+        fontSize: 18,
+    },
+    txtInfo:{
+        color: "#50D9EA",
+        fontSize: 12,
+        marginTop:2
+    },
+    txtTitleSub:{
+        color: 'rgba(0,0,0,0.7)',
+        fontWeight:'bold',
+        fontSize:25,
+        marginTop: 20
+    }
+});
